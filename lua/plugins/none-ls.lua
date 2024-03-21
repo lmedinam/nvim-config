612938d027f2b8df1b26fb95a3642520ffe662f5
Luis Medina <git@lmedinam.es>
2024-03-21 09:03:17 -0400

    Fix `blade-formatter` on Windows

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -31,10 +31,24 @@ return {
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
+			debug = true,
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.pint,
 				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.blade_formatter.with({
+					generator_opts = {
+						command = "blade-formatter",
+						args = function(params)
+							return {
+								"--write",
+								params.temp_path:gsub("\\", "/"), -- Fix filepath for Windows
+							}
+						end,
+					},
+					to_stdin = false,
+					to_temp_file = true,
+				}),
 			},
 			on_attach = on_attach,
 			default_timeout = 5000,
