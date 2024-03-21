99e2718b771e72642aa439c1d26aa216fc91889d
Luis Medina <git@lmedinam.es>
2024-03-21 10:27:24 -0400

    Fix formater map and use only `none-ls`

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,30 +1,3 @@
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
--- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				lsp_formatting(bufnr)
-			end,
-		})
-	end
-end
-
 return {
 	"nvimtools/none-ls.nvim",
 	config = function()
@@ -50,11 +23,7 @@ return {
 					to_temp_file = true,
 				}),
 			},
-			on_attach = on_attach,
-			default_timeout = 5000,
+			default_timeout = 15000,
 		})
-
-		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-		vim.keymap.set("v", "<leader>fm", vim.lsp.buf.format, {})
 	end,
 }
